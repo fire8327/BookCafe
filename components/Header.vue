3c952128162ca958fb7305c1177e95b0fdd09a56
fire8327 <fire83274@gmail.com>
2025-03-28 12:46:55 +0300

    create header

---
+++ components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="w-full grid-container py-4 font-medium">
+        <div class="flex items-center justify-between w-full rounded-xl shadow-[0px_0px_20px_-13px_black] py-2 bg-white px-4">
+            <NuxtLink to="/" class="flex items-center gap-2 transition-all duration-500 hover:opacity-70">
+                <Icon class="text-3xl text-sky-500" name="material-symbols:book"/>
+                <span class="font-semibold text-2xl font-mono">BookCafe</span>
+            </NuxtLink>
+            <div class="flex items-center gap-6">
+                <form class="relative">
+                    <input type="text" class="py-1.5 pl-4 pr-10 rounded-lg border border-gray-200 focus:outline-none">
+                    <button class="cursor-pointer absolute top-1/2 -translate-y-1/2 right-2 flex">
+                        <Icon class="text-3xl text-sky-500" name="material-symbols-light:search"/>
+                    </button>
+                </form>
+                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-sky-500 after:transition-all after:duration-500 hover:after:w-full">Меню</NuxtLink>
+                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-sky-500 after:transition-all after:duration-500 hover:after:w-full">О нас</NuxtLink>
+                <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-sky-500 after:transition-all after:duration-500 hover:after:w-full">Новости</NuxtLink>
+                <div class="flex items-center gap-2">
+                    <NuxtLink to="/" class="transition-all duration-500 hover:opacity-70">
+                        <Icon class="text-3xl text-sky-500" name="material-symbols:person"/>
+                    </NuxtLink>
+                    <NuxtLink to="/" class="transition-all duration-500 hover:opacity-70">
+                        <Icon class="text-3xl text-sky-500" name="ic:baseline-shopping-basket"/>
+                    </NuxtLink>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,7 @@
 <template>
-    <!-- <Header/> -->
+    <Header/>
     <main class="grid-container gap-y-14 md:gap-y-20 xl:gap-y-28 grow my-10">
         <slot/>        
-        <Separator></Separator>
     </main>
    <!--  <Footer/> -->
 </template> 
