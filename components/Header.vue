3b76428255c3fba572de1cd6fac4a78e2342ffb4
fire8327 <fire83274@gmail.com>
2025-03-28 13:11:08 +0300

    create hero, advantages

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <template>
-    <header class="w-full grid-container py-4 font-medium">
-        <div class="flex items-center justify-between w-full rounded-xl shadow-[0px_0px_20px_-13px_black] py-2 bg-white px-4">
+    <header class="w-full grid-container py-4 font-medium text-xl">
+        <div class="flex items-center justify-between w-full rounded-xl shadow-[0px_0px_20px_-13px_black] py-2 bg-white px-4 border border-gray-200">
             <NuxtLink to="/" class="flex items-center gap-2 transition-all duration-500 hover:opacity-70">
                 <Icon class="text-3xl text-sky-500" name="material-symbols:book"/>
-                <span class="font-semibold text-2xl font-mono">BookCafe</span>
+                <span class="font-semibold text-2xl font-mono text-[#131313]/80">BookCafe</span>
             </NuxtLink>
-            <div class="flex items-center gap-6">
+            <div class="flex items-center gap-6 max-lg:hidden">
                 <form class="relative">
                     <input type="text" class="py-1.5 pl-4 pr-10 rounded-lg border border-gray-200 focus:outline-none">
                     <button class="cursor-pointer absolute top-1/2 -translate-y-1/2 right-2 flex">
@@ -24,6 +24,9 @@
                     </NuxtLink>
                 </div>
             </div>
+            <button class="cursor-pointer lg:hidden">
+                <Icon class="text-3xl text-sky-500" name="ic:round-menu"/>
+            </button>
         </div>
     </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,39 @@
 <template>
-    <div>
-        123
+    <div class="relative w-full overflow-hidden rounded-xl">
+        <img src="/images/hero/main.jpg" alt="" class="w-full h-full object-cover object-center">
+        <div class="absolute inset-0 bg-black/30"></div>
+        <div class="absolute inset-0 py-10 px-4 flex justify-center items-center text-center text-white">
+            <div class="flex flex-col items-center relative gap-6 w-fit p-4 lg:p-6 max-w-2xl">
+                <div class="absolute inset-0 backdrop-blur-md rounded-xl"></div>
+                <p class="text-4xl font-mono font-semibold z-[1]">Книги. Кофе. Вдохновение.</p>
+                <p class="opacity-70 text-lg tracking-wide z-[1]">
+                    Уютное пространство, где страницы оживают вместе с ароматом свежего кофе.
+                    Здесь можно провести время за любимой книгой, открыть для себя новые истории.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col gap-6">
+        <p class="mainHeading">Причины выбрать Book Café</p>
+        <p>Здесь время течет иначе: минуты за книгой кажутся часами, а часы за кофе — мгновениями</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div class="flex flex-col gap-4 p-4 rounded-xl bg-white border border-gray-200 shadow-[0px_0px_20px_-15px_black]">
+                <p class="text-2xl font-mono font-semibold text-[#131313]">Библиотека 📚</p>
+                <p>Уютные залы с диванами и полками книг, которые можно читать бесплатно.</p>
+            </div>
+            <div class="flex flex-col gap-4 p-4 rounded-xl bg-white border border-gray-200 shadow-[0px_0px_20px_-15px_black]">
+                <p class="text-2xl font-mono font-semibold text-[#131313]">Авторский кофе ☕️</p>
+                <p>Эксклюзивные смеси от местных обжарщиков и сезонные напитки с литературными названиями.</p>
+            </div>
+            <div class="flex flex-col gap-4 p-4 rounded-xl bg-white border border-gray-200 shadow-[0px_0px_20px_-15px_black]">
+                <p class="text-2xl font-mono font-semibold text-[#131313]">Ивенты 📖</p>
+                <p>Еженедельные чтения, встречи с авторами и тематические вечера.</p>
+            </div>
+            <div class="flex flex-col gap-4 p-4 rounded-xl bg-white border border-gray-200 shadow-[0px_0px_20px_-15px_black]">
+                <p class="text-2xl font-mono font-semibold text-[#131313]">Коворкинг 💻</p>
+                <p>Тихие уголки с розетками и быстрым Wi-Fi для продуктивной работы.</p>
+            </div>
+        </div>
     </div>
 </template>
 
