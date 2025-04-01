11638a75980606f2d1f59e020fb3ae6c1dfae444
fire8327 <fire83274@gmail.com>
2025-04-01 18:41:59 +0300

    created add to cart

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -2,41 +2,129 @@
     <div class="flex flex-col gap-6">
         <p class="mainHeading">Меню</p>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-            <div class="w-full flex flex-col rounded-xl bg-white overflow-hidden transition-all duration-500 shadow-md group hover:shadow-lg hover:-translate-y-2">
+            <div v-for="product in products"
+                class="w-full flex flex-col rounded-xl bg-white overflow-hidden transition-all duration-500 shadow-md group hover:shadow-lg hover:-translate-y-2">
                 <div class="relative">
-                    <img src="/images/about/main.jpg" alt="" class="aspect-video object-cover w-full transition-all duration-500 group-hover:scale-110">
-                    <p class="absolute top-2 left-2 bg-sky-500 text-white text-xs font-semibold py-1 px-4 rounded-full">Хит сезона</p>
+                    <img :src="`https://hsstubxrevlevagsvktq.supabase.co/storage/v1/object/public/images/products/${product.image}`" alt=""
+                        class="aspect-[3/4] object-cover w-full transition-all duration-500 group-hover:scale-105">
+                    <p v-if="product.is_bestseller"
+                        class="absolute top-2 left-2 bg-sky-500 text-white text-xs font-semibold py-1 px-4 rounded-full">
+                        Хит сезона</p>
                     <div class="absolute top-2 right-2 flex items-center gap-1 p-1 rounded-lg bg-gray-800">
-                        <Icon class="text-lg text-white" name="material-symbols:book-2-rounded"/>
-                        <p class="text-xs font-medium text-white">Вишнёвый сад</p>
+                        <Icon class="text-lg text-white" name="material-symbols:book-2-rounded" />
+                        <p class="text-xs font-medium text-white">{{ product.book_pair.title }}</p>
                     </div>
-                    <NuxtLink to="/" class="absolute bottom-2 right-2 text-white transition-all duration-500 hover:opacity-70">
-                        <Icon class="text-3xl" name="material-symbols:eye-tracking-rounded"/>
+                    <NuxtLink :to="`/catalog/product-${product.id}`"
+                        class="absolute bottom-2 right-2 text-white transition-all duration-500 hover:opacity-70">
+                        <Icon class="text-3xl" name="material-symbols:eye-tracking-rounded" />
                     </NuxtLink>
                 </div>
-                <div class="flex flex-col gap-4 p-4">
-                  <div class="flex flex-col">
-                    <p class="text-2xl font-mono font-semibold text-[#131313]/80">Вишнёвый латте</p>
-                    <p class="bg-sky-100 text-sky-800 text-xs px-2 py-1 rounded-md w-fit">Кофейные напитки</p>
-                  </div>
-                  <p class="line-clamp-2">Нежный латте с домашним вишнёвым сиропом, взбитым молоком и нотками миндаля. Подаётся с долькой тёмного шоколада.</p>
-                  <div class="flex items-center gap-2">
-                    <button class="px-4 py-1 text-xs border border-sky-300 rounded-full hover:bg-sky-50 transition-colors">250 мл</button>
-                    <button class="px-4 py-1 text-xs bg-sky-100 text-sky-800 border border-sky-300 rounded-full font-medium">350 мл</button>
-                    <button class="px-4 py-1 text-xs border border-sky-300 rounded-full hover:bg-sky-50 transition-colors">450 мл</button>
-                  </div>
-                  <div class="flex items-center justify-between">
-                    <p class="text-sky-800 font-semibold text-xl font-mono">320 ₽</p>
-                    <button class="cursor-pointer flex items-center gap-2 rounded-xl py-1.5 px-4 transition-all duration-500 bg-sky-600 hover:bg-sky-800 text-white">
-                      <span>Добавить</span>
-                    </button>
-                  </div>
+                <div class="flex flex-col gap-4 p-4 grow">
+                    <p class="text-xl font-mono font-semibold text-[#131313]/80">{{ product.name }}</p>
+                    <p class="bg-sky-100 text-sky-800 text-xs px-2 py-1 rounded-md w-fit mt-auto">{{ product.category }}</p>
+                    <p class="line-clamp-2">{{ product.description }}</p>
+                    <div class="flex items-center gap-2">
+                        <button v-for="priceOption in product.prices" :key="priceOption.volume"
+                            @click="changeVolume(product, priceOption.volume)"
+                            class="px-4 py-1 text-xs border border-sky-300 rounded-full hover:bg-sky-50 transition-all duration-500"
+                            :class="{
+                                'bg-sky-100 text-sky-800 font-medium': product.selectedVolume === priceOption.volume,
+                                'hover:bg-sky-50': product.selectedVolume !== priceOption.volume
+                            }">{{ priceOption.volume }}</button>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <p class="text-sky-800 font-semibold text-xl font-mono">{{ product.selectedPrice.toLocaleString() }} ₽</p>
+                        <button @click="addCart(product)" v-if="authenticated"
+                            class="cursor-pointer flex items-center gap-2 rounded-xl py-1.5 px-4 transition-all duration-500 bg-sky-600 hover:bg-sky-800 text-white w-fit">
+                            <Icon class="text-2xl" name="material-symbols:add" />
+                            <span>Добавить</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    
 </template>
 
 <script setup>
+/* название и язык страницы */
+useSeoMeta({
+    title: 'Меню',
+    lang: 'ru'
+})
 
+
+/* создание сообщений */
+const { showMessage } = useMessagesStore()
+
+
+/* подключение БД */
+const supabase = useSupabaseClient()
+
+const products = ref([])
+
+// загрузка товаров
+const loadProducts = async () => {
+  const { data } = await supabase.from('products').select('*');
+  
+  products.value = data.map(product => ({
+    ...product,
+    prices: typeof product.prices === 'string' ? JSON.parse(product.prices) : product.prices,
+    selectedVolume: product.prices[0]?.volume || '',
+    selectedPrice: product.prices[0]?.price || 0
+  }))
+}
+
+onMounted(() => {
+  loadProducts()
+})
+
+/* изменение объема */
+const changeVolume = (product, volume) => {
+  const productIndex = products.value.findIndex(p => p.id === product.id)
+  if (productIndex === -1) return
+  
+  const priceOption = products.value[productIndex].prices.find(opt => opt.volume === volume)
+  if (!priceOption) return
+  
+  products.value[productIndex].selectedVolume = volume
+  products.value[productIndex].selectedPrice = priceOption.price
+}
+
+
+/* добавление в корзину и проверка входа */
+const { id, authenticated } = storeToRefs(useUserStore())
+const addCart = async (product) => {
+    const { data: carts } = await supabase
+    .from('cart')
+    .select(`*`)
+    .eq('status', 'В корзине')
+    .eq('userId', id.value)
+    .eq('productId', product.id)
+    .eq('volume', product.selectedVolume)
+
+    if (carts && carts.length > 0) {
+        await supabase
+        .from('cart')
+        .update({ count: `${Number(carts[0].count) + 1}` })
+        .eq('id', carts[0].id)
+        .select()
+
+        showMessage("Количество обновлено!", true)
+    } else {
+        const { data, error } = await supabase
+        .from('cart')
+        .insert([
+            { userId: id.value, productId: product.id, status: 'В корзине', count: 1, price: product.selectedPrice, volume: product.selectedVolume},
+        ])
+        .select()
+
+        if (error) {
+            showMessage("Произошла ошибка!", false)
+        } else {
+            showMessage("Добавлено в корзину!", true)
+        }
+    }
+}
 </script>
--- pages/catalog/product-[id].vue
+++ pages/catalog/product-[id].vue
@@ -1,9 +1,119 @@
 <template>
-    <div>
-
+    <div class="flex flex-col gap-6 w-full" v-for="product in processedProducts">
+        <div class="flex items-center text-sm gap-2 font-medium">
+            <NuxtLink to="/catalog" class="transition-all duration-500 hover:text-sky-600">Меню</NuxtLink>
+            <p>/</p>
+            <p class="text-sky-800">{{ product.name }}</p>
+        </div>
+        <div class="flex max-lg:flex-col lg:items-start gap-8 w-full">
+            <img :src="`https://hsstubxrevlevagsvktq.supabase.co/storage/v1/object/public/images/products/${product.image}`" alt="" class="w-full lg:w-1/2 aspect-[7/8] object-cover rounded-xl">
+            <div class="flex flex-col gap-4 w-full lg:w-1/2">
+                <div class="flex items-center justify-between gap-4">
+                    <p class="mainHeading">{{ product.name }}</p>
+                    <p v-if="product.is_bestseller" class="bg-sky-100 text-sky-800 text-sm px-4 py-1 rounded-full">Хит сезона</p>
+                </div>
+                <p class="text-sky-800 font-semibold text-2xl font-mono">{{ product.selectedPrice }} ₽</p>
+                <div class="flex flex-col gap-2">
+                    <p class="font-semibold text-2xl font-mono text-[#131313]/80">Описание</p>
+                    <p>{{ product.description }}</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <p class="font-semibold text-2xl font-mono text-[#131313]/80">Объём</p>
+                    <div class="flex items-center gap-2">
+                        <button v-for="priceOption in product.prices" :key="priceOption.volume"
+                            @click="changeVolume(product, priceOption.volume)"
+                            class="px-4 py-1 text-xs border border-sky-300 rounded-full hover:bg-sky-50 transition-all duration-500"
+                            :class="{
+                                'bg-sky-100 text-sky-800 font-medium': product.selectedVolume === priceOption.volume,
+                                'hover:bg-sky-50': product.selectedVolume !== priceOption.volume
+                            }">{{ priceOption.volume }}</button>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-2 p-4 bg-sky-50 border border-sky-200 rounded-lg">
+                    <p class="font-semibold font-mono text-[#131313]/80">Книжная пара</p>
+                    <p>{{ product.book_pair.description }}</p>   
+                </div>
+                <button class="cursor-pointer flex items-center gap-2 rounded-xl py-1.5 px-10 transition-all duration-500 bg-sky-600 hover:bg-sky-800 text-white w-fit">
+                    <Icon class="text-2xl" name="material-symbols:add"/>
+                    <span>В корзину</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col gap-6">
+        <p class="mainHeading">Рекомендуем также</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div class="flex flex-col rounded-xl overflow-hidden shadow-md" v-for="product in randomProducts">
+                <img :src="`https://hsstubxrevlevagsvktq.supabase.co/storage/v1/object/public/images/products/${product.image}`" alt="" class="w-full aspect-[7/8] object-cover">
+                <div class="flex flex-col gap-2 p-4">
+                    <p class="font-semibold font-mono text-[#131313]/80">{{ product.name }}</p>
+                    <p class="line-clamp-2 mt-auto">{{ product.description }}</p>
+                    <div class="flex items-center justify-between">
+                        <p class="text-sky-800 font-semibold text-xl font-mono">320 ₽</p>
+                        <button class="cursor-pointer flex text-sky-800 transition-all duration-500 hover:opacity-50">
+                            <Icon class="text-2xl" name="material-symbols:add"/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
+/* название и язык страницы */
+useSeoMeta({
+    title: 'Страница товара',
+    lang: 'ru'
+})
 
+
+/* создание сообщений */
+const { showMessage } = useMessagesStore()
+
+
+/* подключение БД и роут */
+const route = useRoute()
+const supabase = useSupabaseClient()
+
+const { data: products, error } = await supabase
+.from('products')
+.select("*")
+.eq('id', route.params.id)
+
+
+/* обрабатываем каждый продукт, добавляя реактивные свойства */
+const processedProducts = ref(products.map(product => {
+  // парсим prices (может быть строкой или массивом)
+  const pricesArray = typeof product.prices === 'string' 
+    ? JSON.parse(product.prices) 
+    : product.prices;
+  
+  return {
+    ...product,
+    prices: pricesArray,
+    selectedVolume: pricesArray[0]?.volume || '350 мл',
+    selectedPrice: pricesArray[0]?.price || 0
+  }
+}))
+
+
+/* функция изменения объёма для конкретного продукта */
+const changeVolume = (product, volume) => {
+  product.selectedVolume = volume
+  const selectedOption = product.prices.find(item => item.volume === volume)
+  product.selectedPrice = selectedOption?.price
+}
+
+
+/* рекомендации */
+const { data: allProducts, error:randomProductsError } = await supabase
+.from('products')
+.select('*')
+.neq('id', route.params.id)
+
+// Перемешиваем массив на клиенте
+const randomProducts = [...allProducts]
+.sort(() => Math.random() - 0.5)
+.slice(0, 4);
 </script>
