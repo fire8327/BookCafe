539906a366ee3428442f71770ba25dbda368762d
fire8327 <fire83274@gmail.com>
2026-05-23 22:43:13 +0300

    feat: mask

--- formkit.config.js
+++ formkit.config.js
@@ -1,13 +1,19 @@
 import { defineFormKitConfig } from '@formkit/vue'
 import { ru } from '@formkit/i18n'
 import { createAutoAnimatePlugin } from '@formkit/addons'
+import { createProPlugin, mask } from '@formkit/pro'
+
+const proPlugin = createProPlugin('fk-62b0de8f08', {
+  mask, 
+  // any other Pro Inputs
+})
 
 export default defineFormKitConfig({
   // rules: {},
   locales: { ru },
   locale: 'ru',
   plugins: [
-    createAutoAnimatePlugin()
+    createAutoAnimatePlugin, proPlugin
   ]
   // etc. 
 })
